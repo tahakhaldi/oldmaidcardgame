4dd569c17fe2be886fbc3007da3759fdba1de05d
Taha Khaldi <tahakhaldi@hotmail.com>
2017-01-23 19:18:31 -0500

    Two new files

---
+++ ReverseSort.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+public class ReverseSort {
+	public static void main(String[] args){
+		char[] unorderedLetters; 
+		unorderedLetters = new char[]{'l', 'i', 'f', 'e'};
+		reverseSort(unorderedLetters); 
+		for (int i = 0 ; i < unorderedLetters.length; i++ )
+			System.out.println(unorderedLetters[i]);
+	}
+
+	//method that sorts a char array into its reverse alphabetical order
+	public static void reverseSort(char[] values){
+
+		char reverse;
+		for (int i = 0; i < values.length; i++) {
+			for (int j = i + 1; j < values.length; j++) {
+				if (values[i] < values[j]) {
+					reverse = values[i];
+					values[i] = values[j];
+					values[j] = reverse;
+				}
+			}
+		}
+	}
+
+}
